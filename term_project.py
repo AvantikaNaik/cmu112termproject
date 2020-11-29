de6aa1145a701b759b25393ee907ccd5f6f5feac
Avantika Naik <53102988+AvantikaNaik@users.noreply.github.com>
2020-11-29 09:46:59 -0500

    Added player wall check, failed a* code and ghost

--- term_project.py
+++ term_project.py
@@ -81,15 +81,109 @@ def makeMaze(app, width, height):
     backtrackThrough(app,row ,col)
 
 def spawnKey(app):
-    row = random.randint(0,app.rows-1)
-    col = random.randint(0,app.cols-1)
+    row = random.randint(2,app.rows-1)
+    col = random.randint(2,app.cols-1)
     app.keyLocation = (row, col)
+
+# https://mat.uab.cat/~alseda/MasterOpt/AStar-Algorithm.pdf
+def astar(app, startval, endval):
+    # Init start and end nodes, and lists
+    start = Node(None, startval)
+    end = Node(None, endval)
+    
+    openList = []
+    closedList = []
+
+    openList.append(start)
+
+    # Loop until you find end
+    while len(openList) > 0:
+        current = openList[0]
+        bestNode = 0
+        for i in range(len(openList)):
+            if current.cost > openList[i].cost:
+                current = openList[i]
+                bestNode = i
+
+        openList.pop(bestNode)
+        closedList.append(current)
+        # if you're at end, return path (backtrackingly)
+        if current == end:
+            path = []
+            tempcurrent = current
+            while tempcurrent != None:
+                path.append(tempcurrent.position)
+                tempcurrent = tempcurrent.parent
+            return path[::-1]
+
+        children = []
+        print("here")
+        # children are adjacent nodes
+        for (drow, dcol) in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            row = current.position[0] + drow
+            col = current.position[1] + dcol
+            if 0 <= row < app.rows and 0 <= col < app.cols and ghostCanMove(app, drow, dcol, row, col) : 
+                new_node = Node(current, (row,col))
+                children.append(new_node)
+
+        for child in children:
+            for closedChild in closedList:
+                if child != closedChild:
+                    child.distance = current.distance + 1
+                    child.heuristic = ((child.position[0] - end.position[0]) ** 2) + ((child.position[1] - end.position[1]) ** 2)
+                    child.cost = child.distance + child.heuristic
+
+                    for openNode in openList:
+                        if child == openNode and child.distance > openNode.distance:
+                            continue
+                    openList.append(child)
+
+def ghostCanMove(app, drow, dcol, row, col):
+    if drow == 0:
+        if dcol == 1 and not app.gridBlocks[row][col].leftLine:
+            return True
+        elif dcol == -1 and not app.gridBlocks[row][col].rightLine:
+            return True
+    elif dcol == 0:
+        if drow == 1 and not app.gridBlocks[row][col].bottomLine:
+            return True
+        elif drow == -1 and not app.gridBlocks[row][col].topLine:
+            return True
+    return False
+
+def moveGhost(app):
+    print("entered")
+    gRow, gCol = getCell(app, app.ghostX, app.ghostY)
+    pRow, pCol = getCell(app, app.playerX, app.playerY)
+    start = (gRow, gCol)
+    end = (pRow, pCol)
+    print("start:", start, "end", end)
+    print("star")
+    path = astar(app, start, end)
+    print("path:", path)
+    #(app.ghostX, app.ghostY) = path[1]
 
 ########################################################
 # Data Classes and Normal Classes
 ########################################################
 GridBlock = make_dataclass('GridBlock', ['topLine', 'bottomLine', 'leftLine',\
             'rightLine', "row", "col"])
+
+# Part of a* algo 
+class Node():
+    def __init__(self, parent, position):
+        self.parent = parent
+        self.position = position
+
+        self.distance = 0
+        self.heuristic = 0
+        self.cost = 0
+
+    def __eq__(self, other):
+        return (isinstance(other, Node) and self.position == other.position)
+
+    def __repr__(self):
+        return f"Node at {self.position} with f={self.cost}"
 
 ########################################################
 # App stuff
@@ -102,8 +196,8 @@ def appStarted(app):
     app.clues = []
     app.text1= "Start typing..."
 
-    app.rows = 10
-    app.cols = 10
+    app.rows = 5
+    app.cols = 5
     app.margin = 20
 
     app.gridBlocks = make2dList(app.rows, app.cols)
@@ -119,36 +213,83 @@ def appStarted(app):
     app.ghostY  = app.height - app.margin * 2
 
     app.keyLocation = (0, 0)
-    spawnKey(app)
+    #spawnKey(app)
     makeMaze(app, app.rows, app.cols)
 
 def timerFired(app):
+    #ghostMove(app)
     pass
 
-def playerLegal(app):
+def playerLegal(app, oldRow, oldCol):
+    newRow, newCol = getCell(app, app.playerX, app.playerY)
     if (app.playerX - 10 < app.margin or app.playerX + 10 > app.width - app.margin
         or app.playerY - 10 < app.margin or app.playerY + 10 > app.height - app.margin):
         print("hello")
         return False
+    elif not isLegal(app, oldRow, oldCol, newRow, newCol) :
+        print("elsecase")
+        return False
+
     return True
 
+def ghostMove(app):
+    oldRow, oldCol = getCell(app, app.ghostX, app.ghostY)
+    ghostSpeed = 5
+    if app.playerX > app.ghostX:
+        app.ghostX += ghostSpeed
+        newRow, newCol = getCell(app, app.ghostX, app.ghostY)
+        if not isLegal(app, oldRow, oldCol, newRow, newCol):
+            app.ghostX -= ghostSpeed
+    else:
+        app.ghostX -= ghostSpeed
+        newRow, newCol = getCell(app, app.ghostX, app.ghostY)
+        if not isLegal(app, oldRow, oldCol, newRow, newCol):
+            app.ghostX += ghostSpeed
+    if app.playerY > app.ghostY:
+        app.ghostY += ghostSpeed
+        newRow, newCol = getCell(app, app.ghostX, app.ghostY)
+        if not isLegal(app, oldRow, oldCol, newRow, newCol):
+            app.ghostX -= ghostSpeed
+    else:
+        newRow, newCol = getCell(app, app.ghostX, app.ghostY)
+        app.ghostY -= ghostSpeed
+        if not isLegal(app, oldRow, oldCol, newRow, newCol):
+            app.ghostX += ghostSpeed
+        
+def isLegal(app, oldRow, oldCol, newRow, newCol):
+    if newRow == oldRow and newCol == oldCol:
+        return True
+    if newRow < oldRow and app.gridBlocks[newRow][newCol].bottomLine == False:
+        return True
+    if newRow > oldRow and app.gridBlocks[newRow][newCol].topLine == False:
+        return True
+    if newCol > oldCol and app.gridBlocks[newRow][newCol].leftLine == False:
+        return True
+    if newCol < oldCol and app.gridBlocks[newRow][newCol].rightLine == False:
+        return True
+    return False
+        
+
 def keyPressed(app, event):
+    if event.key == "u":
+        moveGhost(app)
     app.playerSpeed = 5
-    if event.key == "Up":
+    oldRow, oldCol = getCell(app, app.playerX, app.playerY)
+    if event.key == "Up" or event.key == "w":
         app.playerY -= app.playerSpeed
-        if not playerLegal(app):
+        if not playerLegal(app, oldRow, oldCol):
             app.playerY += app.playerSpeed
-    elif event.key == "Down":
+    elif event.key == "Down" or event.key == "s":
         app.playerY += app.playerSpeed
-        if not playerLegal(app):
+        if not playerLegal(app, oldRow, oldCol):
             app.playerY -= app.playerSpeed
-    elif event.key == "Left":
+    elif event.key == "Left" or event.key == "a":
         app.playerX -= app.playerSpeed
-        if not playerLegal(app):
+        if not playerLegal(app, oldRow, oldCol):
             app.playerX += app.playerSpeed
-    elif event.key == "Right":
+    elif event.key == "Right" or event.key == "d":
         app.playerX += app.playerSpeed
-        if not playerLegal(app):
+        if not playerLegal(app, oldRow, oldCol):
             app.playerX -= app.playerSpeed
 
     if event.key == "j" and not app.journalEntry:
@@ -173,7 +314,6 @@ def keyPressed(app, event):
 
 
 def mousePressed(app, event):
-    print(event.x, event.y)
     if (app.journalVisible and (app.width//4) < event.x <= (3 * app.width//4) \
         and 10 < event.y < 50):
         print("enteredjournalmode")
